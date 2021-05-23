e5273fbb86073e21b94a9d42b19da6e36e20f47b
sjimen13 <sjimen13@gmail.com>
2021-05-23 09:25:05 -0500

    delete-last-name

--- src/database/entities/warranty.ts
+++ src/database/entities/warranty.ts
@@ -9,46 +9,51 @@ import {
   ManyToMany,
   JoinTable,
   JoinColumn,
-} from 'typeorm';
+} from "typeorm";
 
-import { Aparment } from './aparments';
-import { WarrantyType } from './warrantyType';
-import { Status } from './status';
-import { ContactInfo } from './contractInfo';
-import { Exclude } from 'class-transformer';
+import { Aparment } from "./aparments";
+import { WarrantyType } from "./warrantyType";
+import { Status } from "./status";
+import { ContactInfo } from "./contractInfo";
+import { Exclude } from "class-transformer";
 
-@Entity('warranties')
+@Entity("warranties")
 export class Warranty {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Column({ name: 'close_at' })
+  @Column({ name: "close_at", nullable: true })
   closeAt: Date;
 
+  @Column({ nullable: true, default: null })
+  explanation: string;
+  @Column({ nullable: true, default: false })
+  checked: boolean;
+
   @Exclude()
-  @CreateDateColumn({ type: 'timestamp', name: 'created_at' })
+  @CreateDateColumn({ type: "timestamp", name: "created_at" })
   createdAt: Date;
-  
+
   @Exclude()
-  @UpdateDateColumn({ type: 'timestamp', name: 'updated_at' })
+  @UpdateDateColumn({ type: "timestamp", name: "updated_at" })
   updatedAt: Date;
 
-  @Column({ name: 'client_sign', nullable: true })
+  @Column({ name: "client_sign", nullable: true })
   clientSing: number;
 
   @ManyToOne(() => Aparment, (aparment) => aparment.warranties)
   aparment: Aparment;
-  
+
   @ManyToMany((type) => WarrantyType, { eager: true })
   @JoinTable({
-    name: 'warranty_warranty_type',
+    name: "warranty_warranty_type",
     joinColumn: {
-      name: 'id_warranty',
-      referencedColumnName: 'id',
+      name: "id_warranty",
+      referencedColumnName: "id",
     },
     inverseJoinColumn: {
-      name: 'id_warranty_type',
-      referencedColumnName: 'id',
+      name: "id_warranty_type",
+      referencedColumnName: "id",
     },
   })
   warrantyTypes: WarrantyType[];
